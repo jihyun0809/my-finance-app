12fc057c19619b419294ce8702d8bd6707e32539
jiji <pagwa334@gmail.com>
2024-12-23 12:13:45 +0900

    💄 css추가

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,37 @@
+import styled from "styled-components";
+
 const Detail = () => {
-  return <div></div>;
+  return (
+    <Wrapper>
+      <label>
+        날짜:
+        <input />
+      </label>
+      <label>
+        날짜:
+        <input />
+      </label>
+      <label>
+        날짜:
+        <input />
+      </label>
+      <label>
+        날짜:
+        <input />
+      </label>
+      <button>수정</button>
+      <button>삭제</button>
+      <button>뒤로가기</button>
+    </Wrapper>
+  );
 };
 
 export default Detail;
+
+const Wrapper = styled.div`
+  max-width: 800px;
+  margin: 0px auto;
+  padding: 20px;
+  background-color: rgb(255, 255, 255);
+  border-radius: 16px;
+`;
